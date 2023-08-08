f93649aeda59176958e03cb8db140dba69ab88ee
mariam659 <mariamkhank659@gmail.com>
2023-08-07 22:23:18 -0400

    tests

---
+++ tests/unit/recipe.spec.js
@@ -0,0 +1,26 @@
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/Recipe.vue";
+
+describe("HelloWorld.vue", () => {
+  it("renders title of recipe when passed", () => {});
+
+  it("renders ingredients of recipe when passed", () => {});
+
+  it("renders instructions of recipe when passed", () => {});
+
+  it("displays recipe details when a recipe is clicked", () => {});
+
+  it("it shows the like button", () => {});
+
+  it("it shows the unlike button", () => {});
+
+  it("this button should emit like ", () => {});
+
+  it("this button should emit unlike", () => {});
+
+    it('this button should emit "back to main page"', () => { });
+    
+    it('this button should not emit "share" option', () => { });
+    it('this button should emit "share" option', () => { });
+
+});
